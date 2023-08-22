5e4a39c3410397db3e62cd568d09d7d9e021cf00
1119-DuyNguyen <thanhduy191103@gmail.com>
2023-08-23 01:15:29 +0700

    remove unused table

--- database/migrations/2023_04_04_094057_create_coupons_table.php
+++ database/migrations/2023_04_04_094057_create_coupons_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('coupons', function (Blueprint $table) {
-//            $table->id();
+            $table->id();
             $table->string('name');
             $table->string('code')->unique();
             $table->integer('quantity');
