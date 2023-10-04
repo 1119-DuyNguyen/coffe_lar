23556bc3d8ad5818c88127d48b8908fb4b9dfb70
1119-DuyNguyen <thanhduy191103@gmail.com>
2023-10-04 18:50:21 +0700

    header sticky

--- resources/views/templates/clients/home/cart.blade.php
+++ resources/views/templates/clients/home/cart.blade.php
@@ -5,7 +5,46 @@
     @endphp
 
 @if(!empty($carts) && count($carts)>0)
+    <div class="cart_subtotal priceTotal">
+        <h6>Tổng đơn hàng<span class="theme-cl carsub">{{ currency_format($totalAmount, 'đ') }}</span>
+        </h6>
+        {{--            <div class="not-cart">--}}
+        {{--                    <?php--}}
+        {{--                    $feeship = null;--}}
+        {{--                    $tongtien = Session::get('cart')->totalPrice;--}}
+        {{--                    if (Session::has('feeship') != null) {--}}
+        {{--                        $feeship = Session::get('feeship')->feeship;--}}
+        {{--                    }--}}
+        {{--                    ?>--}}
+        {{--                @if(Session::has('coupon') != null)--}}
+        {{--                    <h6>Mã Giảm Giá<span class="theme-cl"> ---}}
+        {{--                    @if(Session::get('coupon')->loaigiam === 1)--}}
+        {{--                                {{currency_format(Session::get('coupon')->giamgia, '%')}}--}}
 
+        {{--                            @endif--}}
+        {{--                            @if(Session::get('coupon')->loaigiam === 2)--}}
+        {{--                                {{currency_format(Session::get('coupon')->giamgia, 'đ')}}--}}
+        {{--                            @endif--}}
+        {{--                </span></h6>--}}
+        {{--                @endif--}}
+        {{--                @if(Session::has('feeship'))--}}
+        {{--                    <h6>Phí vận chuyển<span class="theme-cl"> +--}}
+        {{--                    {{ currency_format($feeship)}}--}}
+        {{--                </span></h6>--}}
+
+        {{--                @else--}}
+        {{--                    <h6>Phí vận chuyển<span style="font-size: 15px;">--}}
+        {{--                    Không hỗ trợ vận chuyển.--}}
+        {{--                </span></h6>--}}
+        {{--                @endif--}}
+        {{--            </div>--}}
+
+        {{--            <input id="checkFeeship" hidden type="number" value="{{$feeship}}">--}}
+        {{--            <input id="totalPrice" hidden type="number"--}}
+        {{--                   data-price="{{currency_format(Session::get('cart')->totalPrice, 'đ')}}"--}}
+        {{--                   value="">--}}
+        <input id="totalCartQuantity" hidden type="number" value="{{ count($cartList)}}">
+    </div>
     <div class="cart_select_items">
         @foreach($cartList as $key => $cart)
             @php
@@ -45,44 +84,5 @@
         @endforeach
     </div>
 
-        <div class="cart_subtotal priceTotal">
-            <h6>Tổng đơn hàng<span class="theme-cl carsub">{{ currency_format($totalAmount, 'đ') }}</span>
-            </h6>
-{{--            <div class="not-cart">--}}
-{{--                    <?php--}}
-{{--                    $feeship = null;--}}
-{{--                    $tongtien = Session::get('cart')->totalPrice;--}}
-{{--                    if (Session::has('feeship') != null) {--}}
-{{--                        $feeship = Session::get('feeship')->feeship;--}}
-{{--                    }--}}
-{{--                    ?>--}}
-{{--                @if(Session::has('coupon') != null)--}}
-{{--                    <h6>Mã Giảm Giá<span class="theme-cl"> ---}}
-{{--                    @if(Session::get('coupon')->loaigiam === 1)--}}
-{{--                                {{currency_format(Session::get('coupon')->giamgia, '%')}}--}}
 
-{{--                            @endif--}}
-{{--                            @if(Session::get('coupon')->loaigiam === 2)--}}
-{{--                                {{currency_format(Session::get('coupon')->giamgia, 'đ')}}--}}
-{{--                            @endif--}}
-{{--                </span></h6>--}}
-{{--                @endif--}}
-{{--                @if(Session::has('feeship'))--}}
-{{--                    <h6>Phí vận chuyển<span class="theme-cl"> +--}}
-{{--                    {{ currency_format($feeship)}}--}}
-{{--                </span></h6>--}}
-
-{{--                @else--}}
-{{--                    <h6>Phí vận chuyển<span style="font-size: 15px;">--}}
-{{--                    Không hỗ trợ vận chuyển.--}}
-{{--                </span></h6>--}}
-{{--                @endif--}}
-{{--            </div>--}}
-
-{{--            <input id="checkFeeship" hidden type="number" value="{{$feeship}}">--}}
-{{--            <input id="totalPrice" hidden type="number"--}}
-{{--                   data-price="{{currency_format(Session::get('cart')->totalPrice, 'đ')}}"--}}
-{{--                   value="">--}}
-            <input id="totalCartQuantity" hidden type="number" value="{{ count($cartList)}}">
-        </div>
 @endif
--- resources/views/templates/clients/layouts/header.blade.php
+++ resources/views/templates/clients/layouts/header.blade.php
@@ -1,173 +1,173 @@
 <div id="preloader">
     <div class="preloader"><span></span><span></span></div>
 </div>
-<div id="main-wrapper">
 
-    <!-- ============================================================== -->
-    <!-- Top header  -->
-    <!-- ============================================================== -->
-    <!-- Start Navigation -->
-    <div class="header">
-        <!-- Topbar -->
-        <div class="header_topbar dark">
-            <div class="container">
-                <div class="row">
+<!-- ============================================================== -->
+<!-- Top header  -->
+<!-- ============================================================== -->
+<!-- Start Navigation -->
+<div class="header">
+    <!-- Topbar -->
+    <div class="header_topbar dark">
+        <div class="container">
+            <div class="row">
 
-                    <div class="col-lg-9 col-md-6 col-sm-6 col-4">
+                <div class="col-lg-9 col-md-6 col-sm-6 col-4">
                         <span class="t-14 ">
                             {{$settings->name}} - ĐỊA CHỈ: {{ $settings->contact_address ?? ""}} - ĐIỆN THOẠI:
                             {{$settings->contact_phone ?? ""}}
                         </span>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-8">
-                        <div class="topbar_menu">
-                            <ul>
-                                <li><a href="#"><i class="fab fa-youtube"></i>Youtube</a></li>
-                                @if(get_user('customer'))
-                                    <li class="hide-m"><a href="{{ route('get.infouser', 'wishlist')}}"><i
-                                                class="fas fa-heart"></i>Yêu thích</a></li>
-                                @endif
-                            </ul>
-                        </div>
-                    </div>
-
                 </div>
-            </div>
-        </div>
-
-        <!-- Main header -->
-        <div class="header_nav" style="padding: 0.75rem 0;">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-2 col-md-2 col-sm-3 col-4">
-                        <a class="nav-brand" href="{{ route('home')}}">
-                            <img src="{{ asset('img/logo.png')}}" class="logo" alt=""/>
-                        </a>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-8">
+                    <div class="topbar_menu">
+                        <ul>
+                            <li><a href="#"><i class="fab fa-youtube"></i>Youtube</a></li>
+                            @if(get_user('customer'))
+                                <li class="hide-m"><a href="{{ route('get.infouser', 'wishlist')}}"><i
+                                            class="fas fa-heart"></i>Yêu thích</a></li>
+                            @endif
+                        </ul>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-4 col-3">
-{{--                        <nav id="navigation" class="navigation navigation-landscape">--}}
-{{--                            <div class="nav-header">--}}
-{{--                                <div class="nav-toggle"></div>--}}
-{{--                            </div>--}}
-{{--                            <div class="nav-menus-wrapper" style="transition-property: none;">--}}
-{{--                                <ul class="nav-menu">--}}
-
-
-
-{{--                                    --}}{{--                                    <li><a href="{{ route('about')}}">Cửa hàng<span--}}
-{{--                                    --}}{{--                                                class="submenu-indicator"></span></a>--}}
-
-{{--                                    --}}{{--                                    </li>--}}
-
-
-{{--                                    --}}{{--                                    <li><a href="{{ route('get.all.promotion')}}">Khuyến mãi<span--}}
-{{--                                    --}}{{--                                                class="submenu-indicator"></span></a>--}}
-{{--                                    --}}{{--                                        <!-- <ul class="nav-dropdown nav-submenu">--}}
-{{--                                    --}}{{--                                            <li><a href="blog.html">...</a></li>--}}
-
-{{--                                    --}}{{--                                        </ul> -->--}}
-{{--                                    --}}{{--                                    </li>--}}
-
-{{--                                </ul>--}}
-
-{{--                            </div>--}}
-{{--                        </nav>--}}
-                    </div>
-
-                    <div class="col-lg-4 col-md-4 col-sm-5 col-5">
-                        <div class="general_head_right">
-                            <ul>
-                                <li><a href="" class="dropdow-user border-icon ">
-                                        @if(get_user('customer'))
-                                            <i class="fas fa-user-circle mgr-10"></i>
-                                            <h6 class="text-user text-overflow">
-                                                {{ (get_user('customer','ten')) ? get_user('customer','ten') : 'Khách hàng' }}
-                                            </h6>
-                                        @else
-                                            <i class="fas fa-user-circle"></i>
-                                        @endif
-                                    </a>
-                                    <div class="user-dropdown">
-                                        <i class="fas fa-times dropexit"></i>
-                                        <ul>
-                                            {{--                                            <li>--}}
-                                            {{--                                                <i class="fa fa-clock" aria-hidden="true"></i>--}}
-                                            {{--                                                <a href="{{ route('search.order')}}" class="ml-2">Tra cứu đơn hàng</a>--}}
-                                            {{--                                            </li>--}}
-                                            @if( get_user('customer'))
-                                                <li>
-                                                    <i class="fas fa-user"></i>
-                                                    <a class="ml-2" href=" {{ route('get.infouser', 'info')}}">Thông tin
-                                                        tài
-                                                        khoản</a>
-                                                </li>
-                                                @if(!get_user('customer','type_social'))
-                                                    <li>
-                                                        <i class="fas fa-sync"></i>
-                                                        <a class="ml-2" href="{{ route('change.pass')}}">Đổi mật
-                                                            khẩu</a>
-                                                    </li>
-                                                @endif
-                                                <li>
-                                                    <i class="fas fa-sign-out-alt"></i>
-                                                    <a class="ml-2" href="{{ route('logout')}}">Đăng xuất</a>
-                                                </li>
-                                            @else
-
-                                                <li>
-                                                    <i class="fas fa-user"></i>
-                                                    <a class="ml-2 " data-toggle="modal" data-target="#login">Đăng
-                                                        nhập</a>
-                                                </li>
-
-                                            @endif
-
-
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li>
-                                    <a class="border-icon" href="javascript:void(0);" data-toggle="modal"
-                                       data-target="#form-search" id="header-search">
-                                        <i class="fas fa-search"></i>
-                                    </a>
-
-
-                                </li>
-                                <li>
-                                    @if(Route::currentRouteName() !== "get.cart")
-                                        <a class="border-icon" href="javascript:void(0);" onclick="openRightMenu()"><i
-                                                class="fas fa-cart-plus"></i><span class="cart_counter"
-                                                                                   id="header-cart-quantity">
-                                            @if(Session::has('cart') != null)
-                                                    {{ count(Session::get('cart')) }}
-                                                @else
-                                                    0
-                                                @endif
-                                        </span></a>
-
-                                    @endif
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="collapse" id="mySearch">
-                            <div class="blocks search_blocks">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Tìm sản phẩm ?">
-                                    <div class="input-group-append">
-                                        <button class="btn search_btn" type="button"><i
-                                                class="fas fa-search"></i></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
+
             </div>
         </div>
     </div>
 
+</div>
+<div class="header header-sticky" >
+
+    <!-- Main header -->
+    <div class="header_nav" style="padding: 0.75rem 0;">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-2 col-md-2 col-sm-3 col-4">
+                    <a class="nav-brand" href="{{ route('home')}}">
+                        <img src="{{ asset('img/logo.png')}}" class="logo" alt=""/>
+                    </a>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-4 col-3">
+                    {{--                        <nav id="navigation" class="navigation navigation-landscape">--}}
+                    {{--                            <div class="nav-header">--}}
+                    {{--                                <div class="nav-toggle"></div>--}}
+                    {{--                            </div>--}}
+                    {{--                            <div class="nav-menus-wrapper" style="transition-property: none;">--}}
+                    {{--                                <ul class="nav-menu">--}}
+
+
+
+                    {{--                                    --}}{{--                                    <li><a href="{{ route('about')}}">Cửa hàng<span--}}
+                    {{--                                    --}}{{--                                                class="submenu-indicator"></span></a>--}}
+
+                    {{--                                    --}}{{--                                    </li>--}}
+
+
+                    {{--                                    --}}{{--                                    <li><a href="{{ route('get.all.promotion')}}">Khuyến mãi<span--}}
+                    {{--                                    --}}{{--                                                class="submenu-indicator"></span></a>--}}
+                    {{--                                    --}}{{--                                        <!-- <ul class="nav-dropdown nav-submenu">--}}
+                    {{--                                    --}}{{--                                            <li><a href="blog.html">...</a></li>--}}
+
+                    {{--                                    --}}{{--                                        </ul> -->--}}
+                    {{--                                    --}}{{--                                    </li>--}}
+
+                    {{--                                </ul>--}}
+
+                    {{--                            </div>--}}
+                    {{--                        </nav>--}}
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-5 col-5">
+                    <div class="general_head_right">
+                        <ul>
+                            <li><a href="" class="dropdow-user border-icon ">
+                                    @if(get_user('customer'))
+                                        <i class="fas fa-user-circle mgr-10"></i>
+                                        <h6 class="text-user text-overflow">
+                                            {{ (get_user('customer','ten')) ? get_user('customer','ten') : 'Khách hàng' }}
+                                        </h6>
+                                    @else
+                                        <i class="fas fa-user-circle"></i>
+                                    @endif
+                                </a>
+                                <div class="user-dropdown">
+                                    <i class="fas fa-times dropexit"></i>
+                                    <ul>
+                                        {{--                                            <li>--}}
+                                        {{--                                                <i class="fa fa-clock" aria-hidden="true"></i>--}}
+                                        {{--                                                <a href="{{ route('search.order')}}" class="ml-2">Tra cứu đơn hàng</a>--}}
+                                        {{--                                            </li>--}}
+                                        @if( get_user('customer'))
+                                            <li>
+                                                <i class="fas fa-user"></i>
+                                                <a class="ml-2" href=" {{ route('get.infouser', 'info')}}">Thông tin
+                                                    tài
+                                                    khoản</a>
+                                            </li>
+                                            @if(!get_user('customer','type_social'))
+                                                <li>
+                                                    <i class="fas fa-sync"></i>
+                                                    <a class="ml-2" href="{{ route('change.pass')}}">Đổi mật
+                                                        khẩu</a>
+                                                </li>
+                                            @endif
+                                            <li>
+                                                <i class="fas fa-sign-out-alt"></i>
+                                                <a class="ml-2" href="{{ route('logout')}}">Đăng xuất</a>
+                                            </li>
+                                        @else
+
+                                            <li>
+                                                <i class="fas fa-user"></i>
+                                                <a class="ml-2 " data-toggle="modal" data-target="#login">Đăng
+                                                    nhập</a>
+                                            </li>
+
+                                        @endif
+
+
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+                                <a class="border-icon" href="javascript:void(0);" data-toggle="modal"
+                                   data-target="#form-search" id="header-search">
+                                    <i class="fas fa-search"></i>
+                                </a>
+
+
+                            </li>
+                            <li>
+                                @if(Route::currentRouteName() !== "get.cart")
+                                    <a class="border-icon" href="javascript:void(0);" onclick="openRightMenu()"><i
+                                            class="fas fa-cart-plus"></i><span class="cart_counter"
+                                                                               id="header-cart-quantity">
+                                            @if(Session::has('cart') != null)
+                                                {{ count(Session::get('cart')) }}
+                                            @else
+                                                0
+                                            @endif
+                                        </span></a>
+
+                                @endif
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="collapse" id="mySearch">
+                        <div class="blocks search_blocks">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Tìm sản phẩm ?">
+                                <div class="input-group-append">
+                                    <button class="btn search_btn" type="button"><i
+                                            class="fas fa-search"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>
 <!-- End Navigation -->
 <div class="clearfix"></div>
