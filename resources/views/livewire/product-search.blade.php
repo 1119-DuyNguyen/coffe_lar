e41c67f18491e97f26720ba61e74aed0cc45ef9e
1119-DuyNguyen <thanhduy191103@gmail.com>
2023-10-09 01:55:27 +0700

    fix css modal and pagination

--- resources/views/livewire/product-search.blade.php
+++ resources/views/livewire/product-search.blade.php
@@ -43,6 +43,7 @@
         @endif
 
     </div>
+    @if($products->count()>0)
     <div class="row">
         <div class="col-12">
 
@@ -50,4 +51,6 @@
         </div>
 
     </div>
+    @endif
+
 </div>
