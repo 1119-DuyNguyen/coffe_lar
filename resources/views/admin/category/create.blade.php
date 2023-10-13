bf86426233754862673101ba9b305881548eb1a0
tienminhvy <tienminhvydev@gmail.com>
2023-10-13 22:15:48 +0700

    Vietsub trang danh sách, thêm, sửa danh mục

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -4,7 +4,7 @@
       <!-- Main Content -->
         <section class="section">
           <div class="section-header">
-            <h1>Category</h1>
+            <h1>Chuyên mục</h1>
           </div>
 
           <div class="section-body">
@@ -13,7 +13,7 @@
               <div class="col-12">
                 <div class="card">
                   <div class="card-header">
-                    <h4>Create Category</h4>
+                    <h4>Khởi tạo chuyên mục</h4>
 
                   </div>
                   <div class="card-body">
@@ -28,17 +28,17 @@
 
                         </div>
                         <div class="form-group">
-                            <label>Name</label>
+                            <label>Tên</label>
                             <input type="text" class="form-control" name="name" value="">
                         </div>
                         <div class="form-group">
-                            <label for="inputState">Status</label>
+                            <label for="inputState">Trạng thái</label>
                             <select id="inputState" class="form-control" name="status">
-                              <option value="1">Active</option>
-                              <option value="0">Inactive</option>
+                              <option value="1">Bật</option>
+                              <option value="0">Tắt</option>
                             </select>
                         </div>
-                        <button type="submit" class="btn btn-primary">Create</button>
+                        <button type="submit" class="btn btn-primary">Khởi tạo</button>
                     </form>
                   </div>
 
