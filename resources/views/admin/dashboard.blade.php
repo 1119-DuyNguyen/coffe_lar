44cd6b4e40f18e634de6004cf8490f7154b086b1
1119-DuyNguyen <thanhduy191103@gmail.com>
2023-10-18 18:32:28 +0700

    remove unused statistic

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,23 +5,23 @@
             <h1>Bảng điều khiển</h1>
         </div>
         <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                <a href="{{ route('admin.order.index') }}">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
-                            <i class="fas fa-cart-plus"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Doanh thu hôm nay</h4>
-                            </div>
-                            <div class="card-body">
-                                {{ 0 }}
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
+{{--            <div class="col-lg-4 col-md-6 col-sm-6 col-12">--}}
+{{--                <a href="{{ route('admin.order.index') }}">--}}
+{{--                    <div class="card card-statistic-1">--}}
+{{--                        <div class="card-icon bg-primary">--}}
+{{--                            <i class="fas fa-cart-plus"></i>--}}
+{{--                        </div>--}}
+{{--                        <div class="card-wrap">--}}
+{{--                            <div class="card-header">--}}
+{{--                                <h4>Doanh thu hôm nay</h4>--}}
+{{--                            </div>--}}
+{{--                            <div class="card-body">--}}
+{{--                                {{ 0 }}--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </a>--}}
+{{--            </div>--}}
             <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                 <a href="{{ route('admin.order.index') }}">
                     <div class="card card-statistic-1">
