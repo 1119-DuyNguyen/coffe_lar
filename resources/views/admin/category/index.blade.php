b943dc34edbb97e2a2397a786814e4da9f853547
1119-DuyNguyen <thanhduy191103@gmail.com>
2023-11-19 20:57:36 +0700

    fix chuyen muc thanh danh muc category

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -4,7 +4,7 @@
       <!-- Main Content -->
         <section class="section">
           <div class="section-header">
-            <h1>Chuyên mục</h1>
+            <h1>Danh mục</h1>
           </div>
 
           <div class="section-body">
@@ -13,7 +13,7 @@
               <div class="col-12">
                 <div class="card">
                   <div class="card-header">
-                    <h4>Danh sách chuyên mục</h4>
+                    <h4>Danh sách danh mục</h4>
 {{--                    <div class="card-header-action">--}}
 {{--                        <a href="{{route('admin.category.create')}}" class="btn btn-primary"><i class="fas fa-plus"></i> Tạo mới</a>--}}
 {{--                    </div>--}}
