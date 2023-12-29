de15a2b5682c6aaf92a0cba219cc91be46effa84
1119-DuyNguyen <thanhduy191103@gmail.com>
2023-12-29 18:05:04 +0700

    update database and version all package

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,14 +42,14 @@ class DatabaseSeeder extends Seeder
             // cà phê đóng gói
             DB::insert("
             INSERT INTO `products` (`id`, `name`, `slug`, `thumb_image`, `category_id`, `description`, `content`, `price`, `status`, `created_at`, `updated_at`) VALUES
-            (". $i+1 . ", 'Tri Ân Thầy Cô {$i}', 'tri-an-thay-co-{$i}', 'uploads/products/tri-an-thay-co.jpg', 1, 'Món quà ý nghĩa ngày nhà giáo', '<p>Khi mua trọn bộ &nbsp;<strong>Tri ân Thầy Cô</strong> Quý khách sẽ được<strong>&nbsp;Tặng kèm 1 phin nhôm nâu và 1 túi giấy đen</strong><br>
+            (". $i+61 . ", 'Tri Ân Thầy Cô {$i}', 'tri-an-thay-co-{$i}', 'uploads/products/tri-an-thay-co.jpg', 1, 'Món quà ý nghĩa ngày nhà giáo', '<p>Khi mua trọn bộ &nbsp;<strong>Tri ân Thầy Cô</strong> Quý khách sẽ được<strong>&nbsp;Tặng kèm 1 phin nhôm nâu và 1 túi giấy đen</strong><br>
             Thời gian khuyến mãi: từ hôm nay đến 30/11/2023 ( hoặc đến khi hết quà tặng).<br>
             </p>
               ', 337000, 1, NULL, '2023-09-23 17:52:28')");
             // bộ quà tặng
             DB::insert("
             INSERT INTO `products` (`id`, `name`, `slug`, `thumb_image`, `category_id`, `description`, `content`, `price`, `status`, `created_at`, `updated_at`) VALUES
-            (". $i+31 . ", 'Bộ Quà Tặng Trung Nguyên Legend {$i}', 'bo-qua-tang-trung-nguyen-legend-{$i}', 'uploads/products/Hop-Qua-ABCD.jpg', 2, 'Món quà ý nghĩa ngày nhà giáo', '
+            (". $i+1 . ", 'Bộ Quà Tặng Trung Nguyên Legend {$i}', 'bo-qua-tang-trung-nguyen-legend-{$i}', 'uploads/products/Hop-Qua-ABCD.jpg', 2, 'Món quà ý nghĩa ngày nhà giáo', '
 
             Từ nơi Đại ngàn Tây Nguyên hùng vĩ – “Vùng đất đỏ bazan huyền thoại, có lịch sử hình thành hơn 160 triệu năm trước”. Câu chuyện Cà phê Chồn bắt đầu với loại chồn hương hoang dã, sống trong những cánh rừng cà phê bạt ngàn… Những “nghệ nhân” này sau khi ăn những trái cà phê chín mọng đã cho ra đời loại cà phê nguyên liệu độc nhất để tạo nên Cà phê WEASEL “vô cùng quý hiếm và đắt nhất thế giới” đến từ Trung Nguyên, với sản lượng hết sức khiêm tốn, chỉ khoảng 40 – 50kg mỗi năm.
 
@@ -58,7 +58,7 @@ class DatabaseSeeder extends Seeder
             // vật phẩm bán lẻ
             DB::insert("
             INSERT INTO `products` (`id`, `name`, `slug`, `thumb_image`, `category_id`, `description`, `content`, `price`, `status`, `created_at`, `updated_at`) VALUES
-            (". $i+61 . ", 'Phin Nhôm Vĩ Nhân {$i}', 'phin-nhom-vi-nhan-{$i}', 'uploads/products/phin-nhom-vi-nhan.png', 3, 'Món quà ý nghĩa ngày nhà giáo', '<p>Thể tích đạt tiêu chuẩn của phin khoảng 170ml.</p>
+            (". $i+31 . ", 'Phin Nhôm Vĩ Nhân {$i}', 'phin-nhom-vi-nhan-{$i}', 'uploads/products/phin-nhom-vi-nhan.png', 3, 'Món quà ý nghĩa ngày nhà giáo', '<p>Thể tích đạt tiêu chuẩn của phin khoảng 170ml.</p>
             <p>Chất liệu nhôm thích hợp nhất cho một chiếc phin lọc cà phê.</p>', 337000, 1, NULL, '2023-09-23 17:52:28')");
         }
         $file_path = [
