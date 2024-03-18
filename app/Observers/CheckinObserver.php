fa4ac8849236d6915fbbf87a5bd6d4c18d41a2c3
Edric <94811667+edricnguyen03@users.noreply.github.com>
2024-03-18 10:13:26 +0700

    update checkin observer

--- app/Observers/CheckinObserver.php
+++ app/Observers/CheckinObserver.php
@@ -12,13 +12,13 @@ class CheckinObserver
     /**
      * Handle the Checkin "created" event.
      */
-    public function created(Checkin $checkin): void
+    public function creating(Checkin $checkin): void
     {
         // dd($checkin);
-        $contract = $checkin->contract;
-        $total_salary = null;
-        $reality_times = 26;
-        if ($contract && $contract->salary) {
+        if ($checkin->contract_id) {
+            $contract = Contract::findOrFail($checkin->contract_id);
+            $total_salary = null;
+            $reality_times = 26;
             $total_salary = ($contract->salary * $reality_times) + ($contract->salary * 2 * $checkin->over_times) - ($checkin->unauth_day_off * $contract->salary);
             if ($checkin->auth_day_off > 12) {
                 $total_salary -= ($checkin->auth_day_off - 12) * $contract->salary;
@@ -27,7 +27,7 @@ class CheckinObserver
             $checkin->reality_times = $reality_times;
             $checkin->total_salary = $total_salary;
             // dd($total_salary);
-            $checkin->save();
+            // $checkin->save();
         }
     }
 
@@ -35,22 +35,20 @@ class CheckinObserver
     /**
      * Handle the Checkin "updated" event.
      */
-    public function updated(Checkin $checkin): void
+    public function updating(Checkin $checkin): void
     {
-        $contract = $checkin->contract;
-        $total_salary = null;
-        $reality_times = 26;
-        $total_salary = ($contract->salary * $reality_times) + ($contract->salary * 2 * $checkin->over_times) - ($checkin->unauth_day_off * $contract->salary);
-        if ($checkin->auth_day_off > 12) {
-            $total_salary -= ($checkin->auth_day_off - 12) * $contract->salary;
+        if ($checkin->contract_id) {
+            $contract = Contract::findOrFail($checkin->contract_id);
+            $total_salary = null;
+            $reality_times = 26;
+            $total_salary = ($contract->salary * $reality_times) + ($contract->salary * 2 * $checkin->over_times) - ($checkin->unauth_day_off * $contract->salary);
+            if ($checkin->auth_day_off > 12) {
+                $total_salary -= ($checkin->auth_day_off - 12) * $contract->salary;
+            }
+            $reality_times = 26 - $checkin->unauth_day_off;
+            $checkin->reality_times = $reality_times;
+            $checkin->total_salary = $total_salary;
         }
-        $reality_times = 26 - $checkin->unauth_day_off;
-        $checkin->reality_times = $reality_times;
-        $checkin->total_salary = $total_salary;
         // dd($total_salary);
-        $checkin->update([
-            'reality_times' => $reality_times,
-            'total_salary' => $total_salary,
-        ]);
     }
 }
