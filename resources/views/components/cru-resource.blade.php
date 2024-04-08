5c0c407dc443021fd31679f3b9bcf00ba5da7789
1119-DuyNguyen <thanhduy191103@gmail.com>
2024-04-08 22:46:34 +0700

    add ajax validate

--- resources/views/components/cru-resource.blade.php
+++ resources/views/components/cru-resource.blade.php
@@ -47,6 +47,114 @@
                                 @endforeach
                                 <button type="submit" class="btn btn-primary">{{$textSubmitData}}</button>
                             </form>
+                            @push('scripts')
+                                <script>
+                                    var forms = document.querySelectorAll("form");
+
+                                    //init span error message
+                                    forms.forEach(form => {
+
+                                        let inputExceptCheckboxes = form.querySelectorAll('input:not([type=checkbox])[name]');
+                                        inputExceptCheckboxes.forEach(input => {
+                                            let parent = input.parentElement;
+                                            let span = document.createElement('div');
+                                            span.innerHTML = `
+                                                <span class="text-danger error-text ${input.name.replace(/\[\]$/, "")}_error"
+                                                style="color: red"></span>`;
+
+                                            parent.insertAdjacentHTML('afterend', span.outerHTML);
+                                        })
+                                        let inputCheckboxes = form.querySelectorAll('input[type=checkbox][name]');
+                                        let distinctInputCheckboxes = {};
+                                        inputCheckboxes.forEach((input) => {
+                                            if (distinctInputCheckboxes[input.name] == null) {
+                                                distinctInputCheckboxes[input.name] = input;
+                                            }
+                                        })
+                                        console.log(inputCheckboxes, distinctInputCheckboxes);
+                                        for (let name in distinctInputCheckboxes) {
+                                            let input = distinctInputCheckboxes[name];
+                                            let parent = input.closest('.row');
+                                            console.log(parent, "hihfishdf")
+                                            let span = document.createElement('div');
+                                            span.innerHTML = `
+                                                <span class="text-danger error-text ${input.name.replace(/\[\]$/, "")}_error"
+                                                style="color: red"></span>`;
+
+                                            parent.insertAdjacentHTML('beforebegin', span.outerHTML);
+                                        }
+
+                                        form.addEventListener('submit', function (e) {
+                                            e.preventDefault();
+
+                                            var all = $(this).serialize();
+                                            $.ajax({
+                                                url: $(this).attr('action'),
+                                                type: "POST",
+                                                data: all,
+                                                beforeSend: function () {
+
+                                                    $(document).find('span.error-text').text('');
+                                                },
+                                                statusCode: {
+                                                    422: function (responseObject, textStatus, jqXHR) {
+                                                        // validation error fails
+                                                        if (responseObject.responseJSON) {
+                                                            let errors = responseObject.responseJSON.errors;
+                                                            // console.log(errors)
+                                                            if (errors) {
+                                                                for (const [prefix, value] of Object.entries(errors)) {
+                                                                    let span = form.querySelector('span.' + prefix + '_error');
+                                                                    if (span) {
+                                                                        span.innerText = value
+                                                                        let input = form.querySelector('input[name=' + prefix + ']');
+                                                                        if (input) input.focus();
+                                                                    }
+
+                                                                }
+
+                                                            }
+                                                        }
+                                                        if (FilamentNotification) {
+                                                            new FilamentNotification()
+                                                                .title('Gửi yêu cầu thất bại')
+                                                                .danger()
+                                                                .send()
+
+                                                        }
+
+                                                    },
+                                                    503: function (responseObject, textStatus, errorThrown) {
+                                                        // Service Unavailable (503)
+                                                        // This code will be executed if the server returns a 503 response
+                                                    }
+                                                },
+                                                success: function (data) {
+                                                    // $('#login').modal('hide');
+                                                    // $('#registerForm').modal('hide');
+                                                    if (FilamentNotification) {
+                                                        new FilamentNotification()
+                                                            .title('Gửi yêu cầu thành công')
+                                                            .success()
+                                                            .send()
+
+                                                    }
+
+
+                                                }
+
+
+                                            })
+
+                                        });
+
+                                    })
+
+
+                                </script>
+
+                            @endpush
+
                         @else
                             Biểu mẫu chưa khởi tạo. Hãy nhấn F5
                         @endif
